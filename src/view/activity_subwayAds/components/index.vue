f64c9cc84a27d2dba6886ad7bd4340ac1dd3373c
Tracy <tracy@groupy.vip>
2017-11-13 15:14:12 +0800

    地铁广告文案修改

--- src/view/activity_subwayAds/components/index.vue
+++ src/view/activity_subwayAds/components/index.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="main">
          <div class="content">
-            <img src="http://photoh5-jp.oss-ap-northeast-1.aliyuncs.com/acticity_banner/activity-books.jpg" class="banner">
+            <img src="http://photoh5-jp.oss-ap-northeast-1.aliyuncs.com/acticity_banner/activity-subwayAdsApply.png" class="banner">
             <h2>{{activity.theme}}</h2>
+            <div class="activity_rule">
+                <p>決勝上位3名のグループは、中国・広州の中心地にある広州塔駅で大型看板に1ヶ月ジャック！1日40万人が通過するところにあなたのグループの写真が掲載されちゃいます！応募は<i>wanted@groupy.vip‍</i> までイベント参加の旨をお伝えください。</p>
+            </div>
             <div class="activity_rule">
                 <h4>{{activity.desc.title}}</h4>
                 <p v-html="activity.desc.p1"></p>
@@ -16,7 +19,7 @@
             <div class="activity_time">
                 <h4>{{activity.times}}</h4>
                 <div class="duration_time">
-                    <p><span>{{activity.start}}</span><i>2017年11月8日（水） 00:00:00</i></p>
+                    <p><span>{{activity.start}}</span><i>2017年11月13日（月） 00:00:00 </i></p>
                     <p><span>{{activity.end}}</span><i>2017年11月30日（木） 23:59:59</i></p>
                 </div>
             </div>
@@ -55,13 +58,6 @@
                     </div>
                 </li>
             </ul>
-            
-            <div class="activity_rule">
-                <h4>{{activity.fansrule.title}}</h4>
-                <p v-html="activity.fansrule.p1"></p>
-                <p v-html="activity.fansrule.p2"></p>
-                <p v-html="activity.fansrule.p3"></p>
-            </div>
             <div class="activity_rule">
                 <h4>{{activity.warning.title}}</h4>
                 <p v-html="activity.warning.p1"></p>
@@ -105,7 +101,7 @@
                 isOver: false,
                 havedMe: false,
                 activity: {
-                    theme:'【Groupy駅看板モデル選定企画】',
+                    theme:'#Groupy広告モデル#',
                     time: 'イベント期間',
                     start: '開始日時',
                     end: '終了日時',
@@ -123,37 +119,31 @@
                     rule: {
                         title: '応募方法',
                         p1: '興味のある方はお気軽に <i>wanted@groupy.vip</i>‍ までスタッフにご連絡ください。',
-                        p2: '<i>※今回のイベントは、ユニット全員のLike数を合算した集計となります（ソロの方も参加可能）</i>'
-                    },
-                    fansrule: {
-                        title: '集計方法',
-                        p1: '1. 全ての参加動画のLike数（ハート）の合計によってランキングが決まります。',
-                        p2: '2. ランキングはイベント詳細ページにリアルタイムで確認できます。',
-                        p3: '3.イベント期間外は、「＃読書の秋」をつけて投稿しても集計されません。'
+                        p2: '<i>※今回のイベントは、ユニット全員のLike数を合算した集計となりますがソロの方も参加可能です。</i>'
                     },
                     desc: {
                         title: '掲載について',
-                        p1: '中国広州の地下鉄は、全市人口の65.8%を占め、一日平均乗降客数の合計が700万人以上！',
-                        p2: '広州タワーも高さ600mの広州代表的な観光地・広州のCBD(中心業務地区)として有名！',
-                        p3: 'そんな賑わう広州塔駅の広告モデルを募集します！',
-                        p4: 'ぜひ中国たくさんの人に顔を覚えてもらえるこの大チャンスをつかんでくだい！',
-                        p5: '<i>※掲載期間：2018年1月1日～2018年1月31日</ii>',
-                        p6: '<i>※掲載場所：広州地下鉄3号線広州塔駅</ii>',
-                        p7: '<i>※サイズ：高さ1,520mm × 幅3,020mm</ii>'
+                        p1: '中国広州の地下鉄は、一日700万人以上が利用し、全市人口の65.8%を占めています！',
+                        p2: 'その中、広州で最も有名な観光スポットである広州タワーの下にあることから、命名された「広州タワー駅」にて、Groupyが広告出稿決定！',
+                        p3: '広州タワーは広州のCBD(中心業務地区)として地元の人だけではなく、海外観光客も多い！1日40万人が通過するところにあなたのグループの写真が掲載されちゃいます！',
+                        p4: '中国の多くの人々に知ってもらえるこの大チャンスをぜひつかんでくだい！',
+                        p5: '<i>※掲載期間：2018年1月1日～2018年1月31日</i>',
+                        p6: '<i>※掲載場所：広州地下鉄3号線広州塔駅</i>',
+                        p7: '<i>※サイズ：高さ1,520mm × 幅3,020mm</i>'
 
                     },
                     warning: {
                         title: '注意事項',
-                        p1: '1. イベント期間中、「#広州塔駅」タグをつけて投稿してください。投稿内容は自由です。',
+                        p1: '1. イベント期間中、<i>「#Groupy広告モデル」</i>タグをつけて投稿してください。投稿内容は自由です。',
                         p2: '2. ユニット全員の参加動画のLike数の合計によってランキングが決まります（ソロの方も参加可能）。',
-                        p3: '3.興味のあるアイドルの方は wanted@groupy.vip‍ までご応募ください。応募せず、予選・決勝期間中「#広州塔駅」をつけて投稿しても無効となります。'
+                        p3: '3.興味のあるアイドルの方は wanted@groupy.vip‍ までご応募ください。応募せず、予選・決勝期間中<i>「#Groupy広告モデル」</i>をつけて投稿しても無効となります。'
                     },
                     rewards: [{
                         p1: '予選特典',
-                        p2: '予選ランキング上位5名かつ50万Like達成のユニット・ソロアイドル：決勝イベントへの参加権を獲得！'
+                        p2: '予選ランキング上位5名かつ50万Like達成のユニット・ソロアイドルは決勝イベントへの参加権を獲得！'
                     }, {
                         p1: '決勝特典',
-                        p2: '決勝ランキング上位3名のユニット・ソロアイドル：中国・広州塔地下鉄駅の大型サインボードに1カ月掲載！'
+                        p2: '決勝ランキング上位3名のユニット・ソロアイドルはGroupy駅広告モデルとして起用！'
                     }]
                 },
                 isFans: true,
